73b1129c3adc429ceffc0f98874076c5986445d3
Xavier De Vleeschauwer <devleesch@gmail.com>
2020-05-25 23:34:13 +0200

    use GPS time

--- message.py
+++ message.py
@@ -6,7 +6,7 @@ class Message:
     def __init__(self, device_id, message: str, timestamp: datetime.datetime):
         self.device_id = device_id
         self.message = message
-        self.datetime = timestamp.utcnow()
+        self.datetime = timestamp
 
     def to_json(self):
         return json.dumps(self.__dict__, default=Message.json_format)
