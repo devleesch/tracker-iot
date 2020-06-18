f044f3e208faa020014b340798011eae69022bdd
De Vleeschauwer Xavier <devleesch@gmail.com>
2020-06-18 11:22:49 +0200

    use isoformat()

--- gps_uart.py
+++ gps_uart.py
@@ -28,7 +28,7 @@ class Gps(Thread):
         # set update rate to 10 times per seconds
         Gps.send_command(gps, bytes('PMTK220,{}'.format(100), "ascii"))
         # open file for csv
-        todayStr = datetime.now().isoformat
+        todayStr = datetime.now().isoformat()
         f = open('csv/'+todayStr+'.csv')
         writer = csv.writer(f)
         while True:
