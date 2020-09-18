093a31760c447f51fef862e5e4710d83e9c50958
Xavier De Vleeschauwer <devleesch@gmail.com>
2020-09-18 10:07:10 +0200

    file format

--- webserver.py
+++ webserver.py
@@ -5,11 +5,13 @@ import os
 
 class WebServer(object):
 
+
     def __init__(self):
         self.env = Environment(
             loader=PackageLoader('webserver', 'templates'),
             autoescape=select_autoescape(['html', 'xml'])
         )
+
 
     @cherrypy.expose
     def index(self):
