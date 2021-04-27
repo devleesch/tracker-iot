0599bc718fac5949481be4b84c83fbe906314d7e
De Vleeschauwer Xavier <devleesch@gmail.com>
2021-04-27 22:11:50 +0200

    use Google Cloud Storage to update instead of git

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 set -x
 
+cd "$(dirname "$0")"
+
 wget https://storage.googleapis.com/tracker-266917.appspot.com/tracker-iot.tar
 tar -xvf tracker-iot.tar
+
+python3 -m venv .venv
+pip install -r requirements.txt
 
 rm tracker-iot.tar
 
