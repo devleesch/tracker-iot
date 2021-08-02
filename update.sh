e2447015b3cde426ce21dabf271ca5ff46d6b94b
De Vleeschauwer Xavier <devleesch@gmail.com>
2021-08-02 21:27:54 +0200

    update requirements.txt and update.sh script

--- update.sh
+++ update.sh
@@ -6,7 +6,8 @@ wget https://storage.googleapis.com/tracker-266917.appspot.com/tracker-iot.tar
 tar -xvf tracker-iot.tar
 
 python3 -m venv .venv
-pip install -r requirements.txt
+source .venv/bin/activate
+pip3 install -r requirements.txt
 
 rm tracker-iot.tar
 
